6c8be55556ff9da3716c70d163d5823bbaab3776
Gabrielle Hardy <gabrhardy@gmail.com>
2016-10-25 17:02:02 -0500

    added letter checking function to js

--- js/word.js
+++ js/word.js
@@ -6,7 +6,21 @@ function Hangman(word, correctGuesses, lettersGuessed, attemptsRemaining, wins, 
 	this.word = word;
 	this.correctGuesses = correctGuesses;
 	this.lettersGuessed = lettersGuessed;
-	this.attemptsRemaining = 0;
+	this.attemptsRemaining = 10;
 	this.wins = 0;
 	this.losses = 0;
 };
+
+if (this.lettersGuessed.indexOf(letter) === -1) {
+    this.lettersGuessed.push(letter);
+
+   for (i = 0; i < this.word.length; i++) {
+        if (this.word[i] === letter) {
+            valid = true;
+            this.validList[i] = letter;
+        }
+    }
+    if (!valid) {
+        this.attemptsRemaining = this.attemptsRemaining - 1;
+    }
+}
