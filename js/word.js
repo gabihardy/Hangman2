1dc689a5a1abe524579aa080c76565f9eb735161
Gabrielle Hardy <gabrhardy@gmail.com>
2016-10-25 10:58:33 -0500

    removed old comments

--- js/word.js
+++ js/word.js
@@ -1,11 +1,7 @@
-/**
- * madlibs.js solution
- **/
 
-// Inquirer dependency
 var inquirer = require("inquirer");
 
-// The madlib constructor
+
 function Hangman(word, correctGuesses, lettersGuessed, attemptsRemaining) {
 	this.word = word;
 	this.correctGuesses = correctGuesses;
