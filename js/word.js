6121e71aafce908f16685cc98ff8abe3d345408e
Gabrielle Hardy <gabrhardy@gmail.com>
2016-10-25 11:09:54 -0500

    minor little fixes and edits

--- js/word.js
+++ js/word.js
@@ -2,9 +2,11 @@
 var inquirer = require("inquirer");
 
 
-function Hangman(word, correctGuesses, lettersGuessed, attemptsRemaining) {
+function Hangman(word, correctGuesses, lettersGuessed, attemptsRemaining, wins, losses) {
 	this.word = word;
 	this.correctGuesses = correctGuesses;
 	this.lettersGuessed = lettersGuessed;
 	this.attemptsRemaining = 0;
+	this.wins = 0;
+	this.losses = 0;
 };
